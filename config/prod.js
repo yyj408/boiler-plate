57c0cfead126ed4d6e46c2c1586bc9fc0dc48187
yunyongjae <yyj960523@gmail.com>
2023-04-27 19:31:16 +0900

    처음 저장소에 올림

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: process.env.MongoURI
-} 
+    mongoURI: process.env.MongoURI,
+};
--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const bodyParser =require("body-parser");
-const config = require("./config/key")
+const express = require('express');
+const app = express();
+const port = 8000;
 
-const {User} = require("./models/User");
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-const mongoose = require("mongoose")
-mongoose.connect(config.mongoURI,{
-  useNewUrlParser: true, useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected..."))
-.catch(err => console.log(err))
-
-
-
-app.get('/', (req, res) => {
-  res.send('새해복 많이 받으세요')
-})
-
-app.post('/register', (req,res) =>{
-  const user = new User(req.body)
-
-
-  user.save()
-  .then(userInfo => {
-    return res.status(200).json({
-      success: true
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb+srv://yyj:abcd1234@cluster0.letdvfe.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
-  })
-  .catch(err => {
-    return res.json({success: false, err})
-  })
-})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
